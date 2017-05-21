a98795075d5b8ef3056612325992cf99e5d7145f
camicarballo <ccarball@nd.edu>
2017-05-20 23:29:33 -0400

    initial commit

---
+++ battery_notification.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+
+# Battery Notification on i3wm 
+# 
+# By default, the script shows three messages:
+#   One at 20%, one at 10%, one at 5% 
+#
+#
+#
+#
+# By Cami Carballo, 2017
