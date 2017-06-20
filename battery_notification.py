ca8bc6700cce1bca97bfb9d806e8bf7663718396
cami <ccarball@nd.edu>
2017-06-20 19:39:30 -0400

    modified

--- battery_notification.py
+++ battery_notification.py
@@ -1,24 +1,23 @@
-#!usr/bin/python
+#!/usr/bin/python2.7
 
 # Low Battery Notification on i3wm 
 # by: Cami Carballo
-# LAST EDIT: 21 May 2017
+# LAST EDIT: 22 May 2017
 
 import os
-
-# set battery variable to location of default battery
-BATTERY = "/org/freedesktop/UPower/devices/battery_BAT0"
+import sys
 
 # check current battery percentage and discharging status
-battery_info = os.popen("upower -i %s", BATTERY, "r").read()
-state = os.popen("cat %s | grep state: | cut -f 2 -d ":" | tr -d [:space:]", battery_info, "r")
-percentage = os.popen("cat %s | grep percentage: | cut -f 2 -d ":" | tr -d [:space:]", battery_info, "r")
+f = os.popen("upower -i /org/freedesktop/UPower/devices/battery_BAT0")
+#battery_info = f.read()
+state = os.popen("cat {} | grep state: | cut -f 2 -d ':' | tr -d [:space:]".format(f))
+#percentage = os.popen("cat {} | grep percentage: | cut -f 2 -d ':' | tr -d [:space:]".format(battery_info))
 
 if state == "discharging":
     # use notify-send to display warning message at 20% and 10%
     if percentage == "20%":
-        os.system("notify-send "BATTERY LOW (20%)" ")
-    elif percentage == "10%":
-        os.system("notify-send "BATTERY LOW (10%)" ")
-    else
-        pass
+        os.system("notify-send 'BATTERY LOW 20%'")
+    #elif percentage == "10%":
+        #os.system("notify-send 'BATTERY LOW 10%'")
+    #else:
+        #os.system("notify-send {}".format(percentage))
