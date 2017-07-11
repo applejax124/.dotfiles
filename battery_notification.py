256f287a6045c032b2eda1362b91024f89c4da10
camicarballo <ccarball@nd.edu>
2017-07-11 17:11:01 -0400

    battery notificationgit add battery_notification.py

--- battery_notification.py
+++ battery_notification.py
@@ -1,23 +1,14 @@
-#!/usr/bin/python2.7
+#!/bin/bash
 
 # Low Battery Notification on i3wm 
 # by: Cami Carballo
-# LAST EDIT: 22 May 2017
+# LAST EDIT: 11 July 2017
 
-import os
-import sys
+BATTINFO=`acpi -b`
 
-# check current battery percentage and discharging status
-f = os.popen("upower -i /org/freedesktop/UPower/devices/battery_BAT0")
-#battery_info = f.read()
-state = os.popen("cat {} | grep state: | cut -f 2 -d ':' | tr -d [:space:]".format(f))
-#percentage = os.popen("cat {} | grep percentage: | cut -f 2 -d ':' | tr -d [:space:]".format(battery_info))
-
-if state == "discharging":
-    # use notify-send to display warning message at 20% and 10%
-    if percentage == "20%":
-        os.system("notify-send 'BATTERY LOW 20%'")
-    #elif percentage == "10%":
-        #os.system("notify-send 'BATTERY LOW 10%'")
-    #else:
-        #os.system("notify-send {}".format(percentage))
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 10% ]
+  ; then
+    notify-send "BATTERY LOW" "$BATTINFO"
+else if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 20% ]]
+  ; then
+    notify-send "BATTERY LOW" "$BATTINFO
