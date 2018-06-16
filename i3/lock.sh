ae2c11c6f32628982fd487c3b430c5fe5461f72c
camicarballo <camigcarballo@gmail.com>
2018-06-16 18:19:00 -0400

    polybar icons, lock screen edits

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,16 +2,14 @@
 
 # Blur Script for i3Lock
 # By: Cami Carballo
-# LAST EDITED: 4 October 2017
+# LAST EDITED: 16 June 2018
 
 
 IMAGE=/tmp/i3lock.png
 SCREENSHOT="scrot $IMAGE" # 0.46s
-ICON=~/.dotfiles/i3/lock-icon.png
 BLURTYPE="5x2" # 3.80s
 
 $SCREENSHOT
 convert $IMAGE -blur $BLURTYPE $IMAGE
-convert $IMAGE $ICON -gravity center -composite -matte $IMAGE
-i3lock -u -i $IMAGE
+i3lock -i $IMAGE
 rm $IMAGE
