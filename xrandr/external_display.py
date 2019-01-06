4b549f114c1bfc3163fc1e329f16a011d573e4f3
camicarballo <camigcarballo@gmail.com>
2019-01-06 17:03:48 -0500

    xrandr & i3: set up xrandr scripts for automatic switch to external displays

---
+++ xrandr/external_display.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+# External Display switch
+# by: Cami Carballo
+# LAST EDIT: 6 January 2019
+
+import sys, os, time
+
+while True:
+
+    edp1 = True
+    dp1 = False
+    dp2 = False
+
+    xrandr_tmp = os.popen("xrandr -q")
+    for line in xrandr_tmp:
+        line = line.split()
+        try:
+            if line[0] == "eDP1":
+                if line[1] == "connected":
+                    edp1 = True
+                else:
+                    edp1 = False
+                if line[2] == "primary":
+                    eprim = True
+                else:
+                    eprim = False
+            elif line[0] == "DP1":
+                if line[1] == "connected":
+                    dp1 = True
+                else:
+                    dp1 = False
+            elif line[0] == "DP2":
+                if line[1] == "connected":
+                    dp2 = True
+                else:
+                    dp2 = False
+        except:
+            pass
+
+    if edp1 and dp1:
+        os.system("xrandr-set HDMI only")
+    elif not eprim and not dp1:
+        os.system("xrandr-set HDMI off")
