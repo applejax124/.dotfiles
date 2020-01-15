899ff1f44eddd0e48c92416bcb162818de70f9ed
camicarballo <camigcarballo@gmail.com>
2020-01-15 08:50:12 -0500

    polybar: vpn configuration

---
+++ polybar/vpnstatus.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+IFACE=`ip address | grep -q cscotun0 | wc -l`
+
+if [ $IFACE -ne 0 ]; then
+    echo "%{F#666}%{F-} vpn connected"
+else
+    echo "%{F#666}%{F-} vpn disconnected"
+fi
